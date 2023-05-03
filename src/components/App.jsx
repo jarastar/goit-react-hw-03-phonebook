2a5ca75f15dff4ec00eacdd8e5ad41fc5faedf87
Taras Yarosevych <121568027+jarastar@users.noreply.github.com>
2023-05-03 12:04:53 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    if (this.isDuplicate(name)) {
+      alert(`${name} is already in contacts.`);
+      return false;
+    }
+
+    this.setState(prevState => {
+      const { contacts } = prevState;
+
+      const newContact = {
+        id: nanoid(4),
+        name,
+        number,
+      };
+
+      return { contacts: [newContact, ...contacts] };
+    });
+
+    return true;
+  };
+
+  handleFilter = ({ target }) => {
+    this.setState({ filter: target.value });
+  };
+
+  getFilteredContacts() {
+    const { filter, contacts } = this.state;
+    if (!filter) {
+      return contacts;
+    }
+
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(normalizedFilter);
+    });
+  }
+
+  deleteContact = id => {
+    this.setState(({ contacts }) => {
+      const newContacts = contacts.filter(contact => contact.id !== id);
+      return { contacts: newContacts };
+    });
+  };
+
+  isDuplicate = name => {
+    const normalizedName = name.toLowerCase();
+
+    const { contacts } = this.state;
+    const contact = contacts.find(({ name }) => {
+      return name.toLowerCase() === normalizedName;
+    });
+
+    return Boolean(contact);
+  };
+
+  render() {
+    const contacts = this.getFilteredContacts();
+    const isContacts = Boolean(contacts.length);
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '20px',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.addContact}
+          checkIsDuplicate={this.isDuplicate}
+        />
+
+        <h2>Contacts</h2>
+        <Filter handleChange={this.handleFilter} />
+        {isContacts ? (
+          <ContactList deleteContact={this.deleteContact} contacts={contacts} />
+        ) : (
+          <p>No contacts in the list. Please add one!</p>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
+
